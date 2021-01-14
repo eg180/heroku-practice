22c7e26e1f11c74b8b649d84cfabe571d1e20745
Erick Gonzalez <ericksinbox@gmail.com>
2021-01-14 15:55:03 -0500

    finished endpoints for update and remove

--- api/posts/post-model.js
+++ api/posts/post-model.js
@@ -31,10 +31,16 @@ function create(post) {
     })
 }
 
-function update() {
-  return Promise.resolve('update wired')
+function update(id, post) {
+  // UPDATE posts SET title = 'foo', contents = 'bar' WHERE id = 1;
+  return db('posts').update(post).where('id', id)
+    .then(() => {
+      return getById(id)
+    })
 }
-
-function remove() {
-  return Promise.resolve('delete wired')
+async function remove(id) {
+  // DELETE FROM posts WHERE id = 1;
+  const postToDelete = await getById(id)
+  await db('posts').delete().where('id', id)
+  return Promise.resolve(postToDelete)
 }
